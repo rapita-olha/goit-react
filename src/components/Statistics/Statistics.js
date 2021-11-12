2bd717f08195a14180fa97b23e1a0866085d32d9
Olha <gorokhovianka@gmail.com>
2021-11-12 15:14:27 +0200

    =

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,18 @@
+import Item from "./Statistics-item";
+import s from "./Statictics.css";
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+
+      <ul className={s.statlist}>
+        {stats.map((item) => (
+          <Item key={item.id} {...item} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
